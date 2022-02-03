d5f2d48d0f2c6488bf579f272d06769fcb937da6
Anjana K G <anjana.kg@experionglobal.com>
2022-02-03 11:07:50 +0530

    added new files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,15 @@ import { NgxPaginationModule } from 'ngx-pagination';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { MedicinesComponent } from './medicines/medicines.component';
+import { MedicineComponent } from './medicines/medicine/medicine.component';
+import { MedicineListComponent } from './medicines/medicine-list/medicine-list.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MedicinesComponent,
+    MedicineComponent,
+    MedicineListComponent
   ],
   imports: [
     BrowserModule,
