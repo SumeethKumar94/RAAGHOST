5e37c803d6a07a07fe12fd2eaceb9d7852b47f1b
Anjana K G <anjana.kg@experionglobal.com>
2022-02-21 17:06:25 +0530

    validations

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -59,13 +59,13 @@ export class EmployeesComponent implements OnInit {
         }
       );
     }
-    
+
     // this.empService.getEmployeeBycontact(this.PhoneNumber).subscribe(
     //   (result) => {
     //     console.log("ph"+result);
     //     alert(" Contact already exist ")
-        
-        
+
+
     //   })
 
   }
@@ -74,116 +74,147 @@ export class EmployeesComponent implements OnInit {
     console.log(form.value);
     let addId = this.empService.formData.EmployeeId;
     //let PhoneNumber = this.empService.formData.PhoneNumber
-    this.PhoneNumber=this.empService.formData.PhoneNumber
+    this.PhoneNumber = this.empService.formData.PhoneNumber
 
 
 
     //insert or update
     if (addId == 0 || addId == null) {
-    //---------------------------------------------------------------------
-    
-    this.empService.getEmployeeBycontact(this.PhoneNumber).subscribe(
-      (result) => {
-        if(result.length>0){
-          alert("Contact already exist")
-           
-        }
-        else{
-          this.insertEmployeeRecord(form); 
-        }
-     
-      })
-    // -------------------------------------------------------------------------------------
-   //this.insertEmployeeRecord(form);  //call
-   //console.log("not set");
+      //---------------------------------------------------------------------
 
-  }
+      this.empService.getEmployeeBycontact(this.PhoneNumber).subscribe(
+        (result) => {
+          if (result.length > 0) {
+            alert("Contact already exist")
+
+          }
+          else {
+            // /////////////////////////////
+            var today = new Date();
+            var nowyear = today.getFullYear();
+            var nowmonth = today.getMonth();
+            var nowday = today.getDate();
+            var b = this.empService.formData.Dob
+
+            var birth = new Date(b);
+
+            var birthyear = birth.getFullYear();
+            var birthmonth = birth.getMonth();
+            var birthday = birth.getDate();
+
+            var age = nowyear - birthyear;
+            var age_month = nowmonth - birthmonth;
+            var age_day = nowday - birthday;
+
+            if (age > 100) {
+              alert("Age cannot be more than 100 Years.Please enter correct age")
+              return false;
+            }
+            if ((age == 18 && age_month <= 0 && age_day <= 0) || age < 18) {
+              alert("Age should be more than 18 years.Please enter a valid Date of Birth");
+              return false;
+            }
+
+
+
+            ///////////////////////////////////////////////////////
+            else {
+              this.insertEmployeeRecord(form);
+            }
+          }
+
+        })
+      // -------------------------------------------------------------------------------------
+      //this.insertEmployeeRecord(form);  //call
+      //console.log("not set");
+
+    }
     else {
+      //update
+      this.updateEmployeeRecord(form);
+    }
+  }
+
+  //insert
+  insertEmployeeRecord(form?: NgForm) {   //declare
+    console.log("inserting a record...");
+    this.empService.insertEmployee(form.value).subscribe(
+      (result) => {
+        console.log(result);
+        this.empId = result;
+        //call resetform for cln the contents
+        this.resetForm(form);
+        this.toastrService.success('Successfully added', 'EmpApp v2022');
+        //this.router.navigateByUrl('/adminhome');
+      },
+      (error) => {
+        console.log(error);
+
+      }
+    )
+
+  }
+  //function
+  insertdoctorRecord(obj: any) {
+    this.empService.insertDoctor(obj).subscribe((result1) => {
+      console.log(result1);
+      this.toastrService.success('Doctor details inserted succesfully', 'EmpApp v2022');
+    }, (error) => {
+      console.log(error);
+
+    });
+  }
+
+
+
   //update
-  this.updateEmployeeRecord(form);
-}
-  }
+  updateEmployeeRecord(form?: NgForm) {
+    console.log("updatinging a record...");
+    this.empService.updateEmployee(form.value).subscribe(
+      (result) => {
+        console.log(result);
+        //this.empId = result;
+        // this.DrObj = { EmployeeId: Number(this.empId), DepId: this.empService.formData.DepId };
+        // this.updatedoctorRecord(this.DrObj);
+        //call resetform for cln the contents
+        this.resetForm(form)
+        this.toastrService.success('Succesfully updated', 'EmpApp v2022');
+      },
+      (error) => {
+        console.log(error);
 
-//insert
-insertEmployeeRecord(form ?: NgForm) {   //declare
-  console.log("inserting a record...");
-  this.empService.insertEmployee(form.value).subscribe(
-    (result) => {
-      console.log(result);
-      this.empId = result;
-      //call resetform for cln the contents
-      this.resetForm(form);
-      this.toastrService.success('Successfully added', 'EmpApp v2022');
-      //this.router.navigateByUrl('/adminhome');
-    },
-    (error) => {
-      console.log(error);
-
-    }
-  )
-
-}
-//function
-insertdoctorRecord(obj: any) {
-  this.empService.insertDoctor(obj).subscribe((result1) => {
-    console.log(result1);
-    this.toastrService.success('Doctor details inserted succesfully', 'EmpApp v2022');
-  }, (error) => {
-    console.log(error);
-
-  });
-}
-
-
-
-//update
-updateEmployeeRecord(form ?: NgForm) {
-  console.log("updatinging a record...");
-  this.empService.updateEmployee(form.value).subscribe(
-    (result) => {
-      console.log(result);
-      //this.empId = result;
-      // this.DrObj = { EmployeeId: Number(this.empId), DepId: this.empService.formData.DepId };
-      // this.updatedoctorRecord(this.DrObj);
-      //call resetform for cln the contents
-      this.resetForm(form)
-      this.toastrService.success('Succesfully updated', 'EmpApp v2022');
-    },
-    (error) => {
-      console.log(error);
-
-    }
-  )
-
-}
-
-updatedoctorRecord(obj: any) {
-  this.empService.updateDoctor(obj).subscribe((result1) => {
-    console.log(result1);
-    this.toastrService.success('Doctor details updated succesfully', 'EmpApp v2022');
-  }, (error) => {
-    console.log(error);
-
-  });
-}
-
-//clear all contents after submit  --initialization
-resetForm(form ?: NgForm) {
-  if (form != null) {
-    form.resetForm();
+      }
+    )
 
   }
-}
 
-navi(){
-  this.router.navigateByUrl('/adminhome');
-}
+  updatedoctorRecord(obj: any) {
+    this.empService.updateDoctor(obj).subscribe((result1) => {
+      console.log(result1);
+      this.toastrService.success('Doctor details updated succesfully', 'EmpApp v2022');
+    }, (error) => {
+      console.log(error);
 
-getToday(): string{
+    });
+  }
 
-  return new Date().toISOString().split('T')[0]
+  //clear all contents after submit  --initialization
+  resetForm(form?: NgForm) {
+    if (form != null) {
+      form.resetForm();
 
-}
+    }
+  }
+
+  navi() {
+    this.router.navigateByUrl('/adminhome');
+  }
+
+  getToday(): string {
+
+    return new Date().toISOString().split('T')[0]
+
+  }
 
 
 }
